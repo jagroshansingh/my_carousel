17a0cd74973c06e69ba4de2cf0a2b9d08f9e9260
Jagroshan <jagroshansingh615@gmail.com>
2023-07-15 21:23:19 +0530

    first simple iteration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from 'react'
 
 function App() {
+  const [count,setCount]=React.useState(0)
+  console.log(count)
+
+  let allImages = [
+    "https://m.media-amazon.com/images/I/71W5IMP9-cL._AC_UL480_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/81wpcDfs7dL._AC._SR360,460.jpg",
+    "https://m.media-amazon.com/images/I/71vxH0htYvL._AC_UL480_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/71QH6ZsXHKL._AC_UL480_FMwebp_QL65_.jpg",
+  ];
+
+  const BtnClick=(direction)=>{
+    if(direction==1) setCount(count+1)
+    else setCount(count-1)
+  }
+
+  React.useEffect(()=>{
+    if(count==4) setCount(0)
+    else if(count==-1) setCount(3)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +38,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <div className="Container">
+        <button onClick={()=>BtnClick(-1)}>Left</button>
+        <div className="ImageContainer">
+          <img
+            alt="image1"
+            src={allImages[count]}
+          />
+        </div>
+        <button onClick={()=>BtnClick(1) }>Right</button>
+      </div>
     </div>
   );
 }
